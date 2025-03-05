c6fefb2ce8568b48b03f9f450a9a4484e184141c
AALOK SHAW <115942969+shawaalok2003@users.noreply.github.com>
2025-03-05 21:47:12 +0530

    new

--- src/components/AchievementsSection.jsx
+++ src/components/AchievementsSection.jsx
@@ -4,6 +4,7 @@ import './AchievementsSection.css'; // Import the CSS file
 const AchievementsSection = () => {
   return (
     <div className="achievements-section">
+      {/* Top Info Section */}
       <div className="top-info">
         <div>
           <span className="new">For kids</span>
@@ -18,27 +19,26 @@ const AchievementsSection = () => {
           <p>4</p>
         </div>
         <div>
-          <span className="new">100+ reviews</span>
+          <span className="new">1000+ reviews</span>
           <p>4.6/5 on Google & FB</p>
         </div>
       </div>
 
+      {/* Achievements Details Section */}
       <div className="achievements-details">
         <div className="achievement-card">
-          
           <div className="logos">
-            <img src="/images/awards.png" alt="Microsoft" />
+            <img src="/images/awards.png" alt="Microsoft Award" />
           </div>
         </div>
         <div className="achievement-card">
-          
           <div className="logos">
             <img src="/images/build.png" alt="India Book of Records" />
           </div>
         </div>
         <div className="achievement-card">
           <div className="logos">
-            <img src="/images/stem.org.png" alt="STEM.org" />
+            <img src="/images/stem.org.png" alt="STEM.org Certified" />
           </div>
         </div>
       </div>
